44f0db63e9acfa90e5477f5f489031a05071f852
MohammedChami <mohamuza@gmail.com>
2024-04-29 09:29:11 +0100

    Add project files.

---
+++ KidStoryAppProject/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace KidStoryAppProject
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if(sender is Button button)
+            {
+                string storyButton = button.Text;
+                Navigation.PushAsync(new Story1(storyButton));
+            }
+        }
+    }
+
+}
